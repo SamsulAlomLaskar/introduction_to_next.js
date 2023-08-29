5a6b0c5ed165d6693dfe7fd07447621c9d431146
Samsul Alom Laskar <laskarikbal126@gmail.com>
2023-08-29 22:57:57 +0530

    navigating through Link tag of next/react for making SPA

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Link from "next/Link";
 
 function News() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS is a great framework of react.js
+          </Link>
+        </li>
+        <li>This has nothing to do</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default News;
