38988b8ce4f84bc1c17456f25b20970a99a69ce1
Samsul Alom Laskar <laskarikbal126@gmail.com>
2023-08-29 22:25:56 +0530

    useRouter hook to get the id/path of the route (similar to the useParams of react-router-dom)

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,4 +1,12 @@
+import { useRouter } from "next/router";
+
 function DetailsPage() {
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
+  // we can send a request to the backend to fetch the news based on the newsId
+
   return <h1>The Details Page</h1>;
 }
 
